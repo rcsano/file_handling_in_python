dff2c2df707c054c478497f079f74a5297a48553
unknown <rc.sanoiz@gmail.com>
2026-04-27 01:41:39 +0800

    added max check, converted GWA to float, and f-string

--- finding_highest_gwa.py
+++ finding_highest_gwa.py
@@ -1,13 +1,23 @@
 # Open the student's record file
 myFile = open("students_record.txt", "r") # Reading
 
+highest = 0
+best = ""
+
 # Loop process line and find to maximum GWA
 for line in myFile:
     # Use split assuming the format 'Name,GWA'
     data = line.strip().split(",")
+    # Added float for math comparison
+    name = data[0]
+    score = float(data[1])
+
+    if score > highest:
+        highest_student = name
+        highest_gwa = score
 
 # Output the result
-print(data)
+print(f"Student with the highest GWA: {highest_student}, {highest_gwa}")
 
 # Close the opened file
 myFile.close()
