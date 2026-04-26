f0fdc0d7a71c71fc7cd4e29f7f633ee1f0be73d5
unknown <rc.sanoiz@gmail.com>
2026-04-27 03:59:04 +0800

    apply oop - added class, state, behavior, and f-string

--- odd_and_even_extractions.py
+++ odd_and_even_extractions.py
@@ -1,20 +1,34 @@
-# Open source and destination files
-input_file = open("numbers.txt", "r") #Reading
-even_file = open("even.txt", "w") #Writing
-odd_file = open("odd.txt", "w") #Writing
+# Add class
+class NumberSorter:
+    def __init__(self, input_file):
+        # State
+        self.input_file = input_file
+        self.even_file = "even.txt"
+        self.odd_file = "odd.txt"
 
-# Use a loop
-for line in input_file:
-    # Remove whitespace and convert into integer
-    number = int(line.strip())
+    # Open source and destination files
+    def sort_numbers(self):
+        # Behavior
+        source = open("numbers.txt", "r")
+        even_file = open("self.even_file", "w")
+        odd_file = open("self.odd_file", "w")
 
-    # Inserted conditional logic
-    if number % 2 == 0:
-        even_file.write(str(number) + "\n")
-    else:
-        odd_file.write(str(number) + "\n")
+    # Use a loop
+        for line in source:
+            # Remove whitespace and convert into integer
+            number = int(line.strip())
+            # Inserted conditional logic
+            if number % 2 == 0:
+                even_file.write(f"{number}\n")
+            else:
+                odd_file.write(f"{number}\n")
 
-# Close all opened files
-input_file.close()
-even_file.close()
-odd_file.close()
+        # Close all opened files
+        source.close()
+        even_file.close()
+        odd_file.close()
+        print("Sorting successful.")
+
+# Instantiation
+sorter = NumberSorter("numbers.txt")
+sorter.sort_numbers()
