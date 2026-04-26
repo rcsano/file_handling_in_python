f127017773dc83d60e7defedc6d20616d3338d65
unknown <rc.sanoiz@gmail.com>
2026-04-27 01:44:03 +0800

    initial value change, flip logic hhehe

--- finding_highest_gwa.py
+++ finding_highest_gwa.py
@@ -1,18 +1,18 @@
 # Open the student's record file
 myFile = open("students_record.txt", "r") # Reading
 
-highest = 0
-best = ""
+highest_student = ""
+highest_gwa = 5.0
 
 # Loop process line and find to maximum GWA
 for line in myFile:
-    # Use split assuming the format 'Name,GWA'
+    # Use split assuming the format 'Name, GWA'
     data = line.strip().split(",")
     # Added float for math comparison
     name = data[0]
     score = float(data[1])
 
-    if score > highest:
+    if score < highest_gwa:
         highest_student = name
         highest_gwa = score
 
