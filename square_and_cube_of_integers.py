c3aa4afa59aa2b7bb750346f91ecbe52f80f2575
unknown <rc.sanoiz@gmail.com>
2026-04-27 04:29:10 +0800

    fixed file handling with open and avoid manually closing files, added exception handling to avoid crash if the file contains invalid data

--- square_and_cube_of_integers.py
+++ square_and_cube_of_integers.py
@@ -6,29 +6,28 @@ class MathProcessor:
         self.double_file = "double.txt"
         self.triple_file = "triple.txt"
 
-    # Instance Method
+    # Instance Method and file handling (avoid manual closing of files)
     def calculate_results(self):
-        source = open(self.input_file, "r")
-        double_file = open(self.double_file, "w")
-        triple_file = open(self.triple_file, "w")
+        with open(self.input_file, "r") as source, \
+             open(self.double_file, "w") as double_file, \
+             open(self.triple_file, "w") as triple_file:
 
-        # Use a loop
-        for line in source:
-            # Remove whitespace and convert into integer
-            number = int(line.strip())
+            # Use a loop and exception handling
+            for line in source:
+                # Remove whitespace and convert into integer
+                try:
+                    number = int(line.strip())
 
-            # Conditional logic
-            if number % 2 == 0:
-                # Square for even
-                double_file.write(f"{number ** 2}\n")
-            else:
-                # Cube for odd
-                triple_file.write(f"{number ** 3}\n")
-
-        # Close all opened files
-        source.close()
-        double_file.close()
-        triple_file.close()
+                    # Conditional logic
+                    if number % 2 == 0:
+                        # Square for even
+                        double_file.write(f"{number ** 2}\n")
+                    else:
+                        # Cube for odd
+                        triple_file.write(f"{number ** 3}\n")
+                except ValueError:
+                    # Skip invalid lines
+                    continue
 
 # Instantiation
 processor = MathProcessor("integers.txt")
