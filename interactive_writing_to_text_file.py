1ddb8b7d05544f0513cb55546c700df29e6c40d0
unknown <rc.sanoiz@gmail.com>
2026-04-27 02:02:56 +0800

    setup

--- interactive_writing_to_text_file.py
+++ interactive_writing_to_text_file.py
@@ -1,8 +1,13 @@
 # Connect and open file
-    # Writing
+def write_to_life():
+    my_file = open("mylife.txt", "w") #Writing
 
 # Get input and write something
+    line = input("Enter line: ")
+    my_file.write(line + "\n")
 
 # Close file
+    my_file.close()
 
-# Execute 
+# Execute
+write_to_life()
