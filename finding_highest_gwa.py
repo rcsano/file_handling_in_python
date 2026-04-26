cd07694fc0f904e0e2e6fcf02c588827872f9cf4
unknown <rc.sanoiz@gmail.com>
2026-04-27 00:56:25 +0800

    setup

--- finding_highest_gwa.py
+++ finding_highest_gwa.py
@@ -1,8 +1,13 @@
 # Open the student's record file
+myFile = open("studentsrecord.txt", "r") # Reading
 
 # Loop process line and find to maximum GWA
+for line in myFile:
     # Use split assuming the format 'Name,GWA'
+    data = line.strip().split(",")
 
 # Output the result
+print(data)
 
 # Close the opened file
+myFile.close()
