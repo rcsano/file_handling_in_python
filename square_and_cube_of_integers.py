b940cbd18afb6e25ed2440895bb89de90e313c76
unknown <rc.sanoiz@gmail.com>
2026-04-27 02:28:26 +0800

    created an outline based from the given algorithm pattern

---
+++ square_and_cube_of_integers.py
@@ -0,0 +1,13 @@
+# Open source file integers.txt
+    #Reading
+    #Writing
+    #Writing
+
+    # Use a loop
+        # Remove whitespace and convert into integer
+
+            # Conditional logic
+
+    # Close all opened files
+
+# Process
