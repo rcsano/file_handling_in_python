db26de0ca47889ab14a390d9680398096425cbd2
unknown <rc.sanoiz@gmail.com>
2026-04-27 00:16:11 +0800

    setup

--- odd_and_even_extractions.py
+++ odd_and_even_extractions.py
@@ -1,8 +1,15 @@
 # Open source and destination files
-    # Reading
-    # Writing
+input_file = open("numbers.txt", "r") #Reading
+even_file = open("even.txt", "w") #Writing
+odd_file = open("odd.txt", "w") #Writing
 
 # Use a loop
+for line in input_file:
     # Remove whitespace and convert into integer
+    number = int(line.strip())
+    print(number)
 
 # Close all opened files
+input_file.close()
+even_file.close()
+odd_file.close()
