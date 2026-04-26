db9b5131504615d7e477a4d1b407af7c2cf160cb
unknown <rc.sanoiz@gmail.com>
2026-04-27 03:00:50 +0800

    setup

--- square_and_cube_of_integers.py
+++ square_and_cube_of_integers.py
@@ -1,13 +1,23 @@
 # Open source file integers.txt
-    #Reading
-    #Writing
-    #Writing
+def process_integers():
+    source = open("integers.txt", "r") #Reading
+    double_file = open("double.txt", "w") #Writing
+    triple_file = open("triple.txt", "w") #Writing
 
     # Use a loop
+    for line in source:
         # Remove whitespace and convert into integer
-
-            # Conditional logic
+        number = int(line.strip())
+        # Conditional logic
+        if number % 2 == 0:
+            double_file.write(str(number ** 2) + "\n") # Square for even
+        else:
+            triple_file.write(str(number ** 3) + "\n") # Cube for odd
 
     # Close all opened files
+    source.close()
+    double_file.close()
+    triple_file.close()
 
 # Process
+process_integers()
