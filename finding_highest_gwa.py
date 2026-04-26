72cfb546ca1b1b9ef1d3898945e8553a88e09066
unknown <rc.sanoiz@gmail.com>
2026-04-27 04:15:02 +0800

    apply oop - added class, instance variable and method, and instatiation

--- finding_highest_gwa.py
+++ finding_highest_gwa.py
@@ -1,23 +1,32 @@
-# Open the student's record file
-students_record = open("students_record.txt", "r") # Reading
+# Add class
+class GradeTracker:
+    def __init__(self, record_file):
+        # Instance Variables
+        self.record_file = record_file
+        self.highest_gwa = 5.0
+        self.top_student = ""
 
-highest_student = ""
-highest_gwa = 5.0
+# Instance Method
+    def find_top_performer(self):
+        students_record = open("students_record.txt", "r") # Reading
+        # Loop process line and find to maximum GWA
+        for line in students_record:
+            # Use split assuming the format 'Name, GWA'
+            name, score = line.strip().split(",")
+            score = float(score)
 
-# Loop process line and find to maximum GWA
-for line in students_record:
-    # Use split assuming the format 'Name, GWA'
-    data = line.strip().split(",")
-    # Added float for math comparison
-    name = data[0]
-    score = float(data[1])
+            if score < self.highest_gwa:
+                self.highest_gwa = score
+                self.top_student = name
 
-    if score < highest_gwa:
-        highest_student = name
-        highest_gwa = score
+        # Output the result
+        print(f"Student with the highest GWA: {self.top_student}, {self.highest_gwa}")
 
-# Output the result
-print(f"Student with the highest GWA: {highest_student}, {highest_gwa}")
+        # Close the opened file
+        students_record.close()
 
-# Close the opened file
-students_record.close()
+# Instantiation
+tracker = GradeTracker("students_record.txt")
+
+# Perform Action
+tracker.find_top_performer()
