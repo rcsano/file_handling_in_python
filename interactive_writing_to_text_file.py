dbfd5870d0da9c6c12b3973214febb6fc05cd6b2
unknown <rc.sanoiz@gmail.com>
2026-04-27 04:43:58 +0800

    applied oop - class, instance variable and method, instantiation, and f-string

--- interactive_writing_to_text_file.py
+++ interactive_writing_to_text_file.py
@@ -1,22 +1,30 @@
-# Connect and open file
-def write_to_life():
-    my_life = open("mylife.txt", "w") #Writing
+# Add class
+class LifeDiary:
+    def __init__(self, output_file):
+        # Instance Variable
+        self.output_file = output_file
 
-    # While loop
-    while True:
-        # Get input and write something
-        line = input("Enter line: ")
-        my_life.write(line + "\n")
+    # Connect and open file
+    def write_to_life(self):
+        my_life = open("self.output_file", "w") #Writing
 
-        # Added yes or no question
-        choice = input("Would you like to write another line? (y/n): ")
+        # While loop
+        while True:
+            # Get input and write something
+            line = input("Enter line: ")
+            my_life.write(f"{line}\n")
 
-        # Break loop if no
-        if choice == "n":
-            break
+            # Added yes or no question
+            choice = input("Would you like to write another line? (y/n): ")
+            # Break loop if no
+            if choice.lower() == "n":
+                break
 
-    # Close the file only if the loop if finished
-    my_life.close()
+        # Close the file only if the loop if finished
+        my_life.close()
 
-# Execute
-write_to_life()
+        print(f"Entries saved to {self.output_file}")
+
+# Instantiation
+diary = LifeDiary("mylife.txt")
+diary.write_to_life()
