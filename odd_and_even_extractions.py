6eaf0e5cfd29468906ab7cd1eb623178e87be384
unknown <rc.sanoiz@gmail.com>
2026-04-27 13:08:35 +0800

    follow the pattern in other programs to have the same flow: fixed file handling with open and avoid manually closing files, added exception handling to avoid crash if the file contains invalid data, and changed other words from the outline

--- odd_and_even_extractions.py
+++ odd_and_even_extractions.py
@@ -1,34 +1,37 @@
 # Add class
 class NumberSorter:
     def __init__(self, input_file):
-        # State
+        # Instance variables
         self.input_file = input_file
         self.even_file = "even.txt"
         self.odd_file = "odd.txt"
 
-    # Open source and destination files
+    # Instance method and file handling (avoid manual closing of files)
     def sort_numbers(self):
-        # Behavior
-        source = open("numbers.txt", "r")
-        even_file = open("self.even_file", "w")
-        odd_file = open("self.odd_file", "w")
+            with open(self.input_file, "r") as source, \
+                 open(self.even_file, "w") as even_file, \
+                 open(self.odd_file, "w") as odd_file:
 
-    # Use a loop
-        for line in source:
-            # Remove whitespace and convert into integer
-            number = int(line.strip())
-            # Inserted conditional logic
-            if number % 2 == 0:
-                even_file.write(f"{number}\n")
-            else:
-                odd_file.write(f"{number}\n")
+                # Use a loop and exception handling
+                for line in source:
+                    # Remove whitespace and convert into integer
+                    try:
+                        number = int(line.strip())
 
-        # Close all opened files
-        source.close()
-        even_file.close()
-        odd_file.close()
-        print("Sorting successful.")
+                        # Inserted conditional logic
+                        if number % 2 == 0:
+                            even_file.write(f"{number}\n")
+                        else:
+                            odd_file.write(f"{number}\n")
+
+                    except ValueError:
+                        # Skip invalid lines
+                        continue
+
+            print("Sorting successful.")
 
 # Instantiation
 sorter = NumberSorter("numbers.txt")
+
+# Perform action
 sorter.sort_numbers()
